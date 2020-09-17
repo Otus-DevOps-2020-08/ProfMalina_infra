80435de3912c3671fe532883b13a177989f27cc5
Roman Malinovskiy <malinovskiyr@inbox.ru>
2020-09-17 11:57:16 +0300

    replase apt to apt-get in *.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Install git"
-sudo apt install -y git
+sudo apt-get install -y git
 echo "Deploy project"
 cd $HOME
 git clone -b monolith https://github.com/express42/reddit.git
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Install ruby"
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
+sudo apt-get update
+sudo apt-get install -y ruby-full ruby-bundler build-essential
