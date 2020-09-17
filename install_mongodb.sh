6215e9f33eeb33fd9d37c8d4fd7c7f1539062079
Roman Malinovskiy <malinovskiyr@inbox.ru>
2020-09-17 11:02:14 +0300

    replace sh to bash in *.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
 touch /etc/apt/sources.list.d/mongodb-org-4.2.list
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 apt update
 apt install -y ruby-full ruby-bundler build-essential
