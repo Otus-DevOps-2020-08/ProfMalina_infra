9d662dbc845f172bc667072c7a4d72e92cdcdb80
Roman Malinovskiy <malinovskiyr@inbox.ru>
2020-10-12 21:26:42 +0300

    add hw with * and packer update

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -13,16 +13,9 @@ resource "local_file" "AnsibleInventory" {
  content = templatefile("inventory.tmpl",
  {
   external_ip_address_app = module.app.external_ip_address_app,
-  external_ip_address_db = module.db.external_ip_address_db
- }
- )
- filename = "../../ansible/inventory.json"
-}
-resource "local_file" "AnsibleApp" {
- content = templatefile("app.tmpl",
- {
+  external_ip_address_db = module.db.external_ip_address_db,
   internal_ip_address_db = module.db.internal_ip_address_db
  }
  )
- filename = "../../ansible/app.yml"
+ filename = "../../ansible/inventory.json"
 }
