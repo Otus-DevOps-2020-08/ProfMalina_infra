51eeeba486dd29a04c819e429b795c2a865a84ef
Roman Malinovskiy <malinovskiyr@inbox.ru>
2020-10-22 14:06:05 +0300

    fix travis?

--- config-scripts/tests.sh
+++ config-scripts/tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -v
 echo 'Packer test'
 packer validate -var-file=packer/variables.json.example packer/app.json
 packer validate -var-file=packer/variables.json.example packer/db.json
